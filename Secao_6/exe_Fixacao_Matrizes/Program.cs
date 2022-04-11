abbcec1c1d9fe78b98c072cff893ee0957d12907
unknown <requestia@DESKTOP-M429FEH.requestia.local>
2022-04-10 22:13:44 -0300

    add exercícios de matrizes

---
+++ Secao_6/exe_Fixacao_Matrizes/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace exe_Aluguel_De_Quartos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Fazer um programa para ler dois números inteiros M e N, e depois ler
+                uma matriz de M linhas por N colunas contendo números inteiros,
+                podendo haver repetições. Em seguida, ler um número inteiro X que
+                pertence à matriz. Para cada ocorrência de X, mostrar os valores à
+                esquerda, acima, à direita e abaixo de X, quando houver, conforme
+                exemplo.
+            */
+
+            Console.WriteLine("Dados da matriz");
+            Console.Write("Informe o número de linhas: ");
+            int linha = int.Parse(Console.ReadLine());
+            Console.Write("Informe o número de colunas: ");
+            int coluna  = int.Parse(Console.ReadLine());
+
+            int [,] mat = new int [linha, coluna];
+
+            Console.WriteLine();
+            for (int i = 0; i < linha; i++)
+            {
+                for (int j = 0; j < coluna; j++)
+                {
+                    Console.Write($"Posição [{i}x{j}]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.Write("\nMatriz:");
+            for (int i = 0; i < linha; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < coluna; j++)
+                {
+                    Console.Write(" " + mat[i, j] + " ");
+                }
+            }
+
+            Console.Write("\n\nInforme um número da matriz para a verificação: ");
+            int numVerif = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            for (int i = 0; i < linha; i++)
+            {
+                for (int j = 0; j < coluna; j++)
+                {
+                    if (mat[i, j] == numVerif)
+                    {
+                        Console.WriteLine($"Posição [{i},{j}] ");
+                        if (j > 0)
+                        {
+                            Console.WriteLine($"Número na esquerda: {mat[i, j - 1]}");
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine($"Número acima: {mat[i - 1, j]}");
+                        }
+                        if (j < coluna - 1)
+                        {
+                            Console.WriteLine($"Número na direita: {mat[i, j + 1]}");
+                        }
+                        if (i < linha - 1)
+                        {
+                            Console.WriteLine($"Número abaixo: {mat[i + 1, j]}");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Secao_6/exe_Matrizes/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace exe_Aluguel_De_Quartos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Fazer um programa para ler um número inteiro N e uma matriz de
+                ordem N contendo números inteiros. Em seguida, mostrar a diagonal
+                principal e a quantidade de valores negativos da matriz.
+            */
+
+            Console.Write("Informe a ordem da matriz: ");
+            int ordemMatriz = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[ordemMatriz, ordemMatriz];
+
+            Console.WriteLine();
+            for (int i = 0; i < ordemMatriz; i++)
+            {
+                for (int j = 0; j < ordemMatriz; j++)
+                {
+                    Console.Write($"Posição [{i}x{j}]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.Write("\nMatriz:");
+            for (int i = 0; i < ordemMatriz; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < ordemMatriz; j++)
+                {
+                    Console.Write(" " + mat[i,j] + " ");
+                }
+            }
+
+            Console.WriteLine("\n\nDiagonal princípal:");
+            for (int i = 0; i < ordemMatriz; i++)
+            {
+                Console.Write(" " + mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < ordemMatriz; i++)
+            {
+                for (int j = 0; j < ordemMatriz; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.Write($"\nQuantidade de números negativos na matriz: {count}\n");
+        }
+    }
+}
